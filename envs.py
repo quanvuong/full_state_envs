d991dab2d28183693f05e21ff04aeffb0308ab19
Quan Vuong <quan.vuong@nyu.edu>
2018-12-14 23:08:15 -0800

    init commit

---
+++ envs.py
@@ -0,0 +1,59 @@
+import numpy as np
+from gym import utils
+from gym.wrappers import TimeLimit
+from gym.envs.mujoco import mujoco_env
+
+
+def make_env(env_id):
+
+    if env_id == 'HalfCheetah-full-state-v2':
+        env = HalfCheetahEnv()
+        max_episode_step = 1000
+    else:
+        exit('Env ID not found')
+
+    env = TimeLimit(env, max_episode_steps=max_episode_step)
+
+    return env
+
+
+class HalfCheetahEnv(mujoco_env.MujocoEnv, utils.EzPickle):
+    def __init__(self):
+        mujoco_env.MujocoEnv.__init__(self, 'half_cheetah.xml', 5)
+        utils.EzPickle.__init__(self)
+
+    def step(self, action):
+        xposbefore = self.sim.data.qpos[0]
+        self.do_simulation(action, self.frame_skip)
+        xposafter = self.sim.data.qpos[0]
+        ob = self._get_obs()
+        reward_ctrl = - 0.1 * np.square(action).sum()
+        reward_run = (xposafter - xposbefore)/self.dt
+        reward = reward_ctrl + reward_run
+        done = False
+        return ob, reward, done, dict(reward_run=reward_run, reward_ctrl=reward_ctrl)
+
+    def _get_obs(self):
+        return np.concatenate([
+            self.sim.data.qpos.flat[1:],
+            self.sim.data.qvel.flat,
+        ])
+
+    def reward_fn(self, state, action, next_state):
+        xposbefore = state[0]
+        xposafter = next_state[0]
+
+        reward_ctrl = - 0.1 * np.square(action).sum()
+        reward_run = (xposafter - xposbefore)/self.dt
+        reward = reward_ctrl + reward_run
+
+        return reward
+
+    def reset_model(self):
+        qpos = self.init_qpos + self.np_random.uniform(low=-.1, high=.1, size=self.model.nq)
+        qvel = self.init_qvel + self.np_random.randn(self.model.nv) * .1
+        self.set_state(qpos, qvel)
+        return self._get_obs()
+
+    def viewer_setup(self):
+        self.viewer.cam.distance = self.model.stat.extent * 0.5
